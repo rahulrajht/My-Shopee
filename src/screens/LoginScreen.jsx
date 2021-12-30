4ad8dd9ff92193cbbdf5a5be03f237720ac93ede
Rahul <rahulrajnkeht@gmail.com>
2021-12-30 20:16:13 +0530

    Added state prop to router

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory, useLocation } from 'react-router-dom'
 import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
@@ -7,15 +7,15 @@ import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
 import { login } from '../actions/userActions'
 
-const LoginScreen = ({ location, history }) => {
+const LoginScreen = ({  history }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const dispatch = useDispatch()
-
   const userLogin = useSelector((state) => state.userLogin)
   const { loading, error, userInfo } = userLogin
+  const location = useLocation()
   
-  const redirect = location.search ? location.search.split('=')[1] : '/'
+  const redirect = location.state ? location.state.from: '/'
 
    useEffect(() => {
     if (userInfo) {
