a4441dc25f8544970dce63aa9728bf194579255d
Rahul <rahulrajnkeht@gmail.com>
2022-01-11 18:37:10 +0530

    fixed footer bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Container, Row, Col, Navbar } from "react-bootstrap";
+import { Container, Col, Navbar } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="fixed-bottom">
       <Navbar expand="lg" variant="dark" bg="dark">
         <Container>
         <Col className='text-center py-3 text-light'>Copyright &copy; My Shopee</Col>
