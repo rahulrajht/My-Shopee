4dcf1a80467449d749a65b23f1d5eb4939bff842
Rahul <rahulrajnkeht@gmail.com>
2022-10-15 16:05:42 +0530

    Fixed typo

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -11,10 +11,7 @@ const RegisterScreen = ({ location, history }) => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const [message, setMessage] = useState(null)
   const dispatch = useDispatch()
-
   const userRegister = useSelector((state) => state.userRegister)
   const { loading, error, userInfo } = userRegister
   const redirect = location.search ? location.search.split('=')[1] : '/'
@@ -26,18 +23,13 @@ const RegisterScreen = ({ location, history }) => {
   }, [history, userInfo, redirect])
 
   const submitHandler = (e) => {
-    e.preventDefault()
-    if (password !== confirmPassword) {
-      setMessage('Passwords do not match')
-    }else {
-      dispatch(register(name, email, password))
-    }
+    e.preventDefault()  
+    dispatch(register(name, email, password))    
   }
 
   return (
     <FormContainer>
       <h1>Sign Up</h1>
-      {message && <Message variant='danger'>{message}</Message>}
       {error && <Message variant='danger'>{error}</Message>}
       {loading && <Loader />}
       <Form onSubmit={submitHandler}>
@@ -62,7 +54,7 @@ const RegisterScreen = ({ location, history }) => {
         </Form.Group>
 
         <Form.Group controlId='password'>
-          <Form.Label>Password Address</Form.Label>
+          <Form.Label>Password</Form.Label>
           <Form.Control
             type='password'
             placeholder='Enter password'
@@ -71,17 +63,7 @@ const RegisterScreen = ({ location, history }) => {
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId='confirmPassword'>
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Confirm password'
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-
-        <Button type='submit' variant='primary' className='mt-2'>
+        <Button type='submit' variant='outline-primary' className='mt-2'>
           Register
         </Button>
       </Form>
