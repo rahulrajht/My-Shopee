abc77146e05f490305e3ec5b305f49069281cb95
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2024-07-14 18:16:03 +0530

    Update productActions.js

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -26,7 +26,7 @@ import {
   HIGH_TO_LOW,
   LOW_TO_HIGH,
 } from "../constants/productConstants";
-const BACKEND_URL = 'https://7b37af4f-20ec-4e30-a996-523a26481b19-00-1mmmp7ap5soe4.kirk.replit.dev';
+const BACKEND_URL = 'https://my-shopee-blond.vercel.app';
 export const listProducts =
   (keyword = "") =>
   async (dispatch) => {
