92d0b8b04f9454e04d7efb06d73cb73ca88141bc
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2024-07-14 18:15:42 +0530

    Update orderActions.js

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -19,7 +19,7 @@ import {
   ORDER_DELIVER_SUCCESS,
   ORDER_DELIVER_REQUEST,
 } from '../constants/orderConstants'
-const BACKEND_URL = 'https://7b37af4f-20ec-4e30-a996-523a26481b19-00-1mmmp7ap5soe4.kirk.replit.dev';
+const BACKEND_URL = 'https://my-shopee-blond.vercel.app';
 export const createOrder = (order) => async (dispatch, getState) => {
   try {
     dispatch({
