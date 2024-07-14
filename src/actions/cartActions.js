d94b47585e456f4c70b98c5f2b626ca9f7193125
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2024-07-14 18:15:14 +0530

    Update cartActions.js

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -11,7 +11,7 @@ import {
   ITEM_ADDED_SUCESS
 } from '../constants/cartConstants'
 
-const BACKEND_URL = 'https://7b37af4f-20ec-4e30-a996-523a26481b19-00-1mmmp7ap5soe4.kirk.replit.dev';
+const BACKEND_URL = 'https://my-shopee-blond.vercel.app';
 
 export const addToCart = (id, qty , auth) => async (dispatch, getState) => {
   dispatch({
